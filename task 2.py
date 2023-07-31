8a99598e481b3a545ec4da36f2e9fe1c46342e55
sergey korolev <rol1987@yandex.ru>
2023-07-31 12:25:02 +0300

    Initial

--- task 2.py
+++ task 2.py
@@ -2,10 +2,11 @@ import types
 
 def flat_generator(list_of_lists):
     main_coursor = 0
-    while main_coursor <= len(list_of_lists):
-        nested_coursor = 0      
+    while main_coursor < len(list_of_lists):
+        nested_coursor = 0    
+        # print(main_coursor)  
         while nested_coursor < len(list_of_lists[main_coursor]):
-            print (main_coursor, nested_coursor)   
+            # print (main_coursor, nested_coursor)   
             yield list_of_lists[main_coursor][nested_coursor]
             nested_coursor += 1
         main_coursor += 1
@@ -23,7 +24,6 @@ def test_2():
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
         assert flat_iterator_item == check_item
-
     assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
